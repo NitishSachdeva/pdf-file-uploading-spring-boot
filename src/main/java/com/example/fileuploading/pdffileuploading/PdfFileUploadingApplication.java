a1c6e0e03cb3543950c77d6df2eea98db167856d
NitishSachdeva <nitishsachdeva39@gmail.com>
2022-10-08 00:48:09 +0530

    Initial project setup

---
+++ src/main/java/com/example/fileuploading/pdffileuploading/PdfFileUploadingApplication.java
@@ -0,0 +1,13 @@
+package com.example.fileuploading.pdffileuploading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfFileUploadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfFileUploadingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fileuploading/pdffileuploading/PdfFileUploadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fileuploading.pdffileuploading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfFileUploadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
