f2686e804b6db26d68ecb4592c399e46e7550cad
NitishSachdeva <nitishsachdeva39@gmail.com>
2022-10-10 00:18:25 +0530

    SWagger Implementation for API documentation purpose

--- src/main/java/com/example/fileuploading/pdffileuploading/PdfFileUploadingApplication.java
+++ src/main/java/com/example/fileuploading/pdffileuploading/PdfFileUploadingApplication.java
@@ -2,12 +2,16 @@ package com.example.fileuploading.pdffileuploading;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableWebMvc
 public class PdfFileUploadingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PdfFileUploadingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PdfFileUploadingApplication.class, args);
+    }
 
 }
